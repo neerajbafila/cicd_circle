f8b8628108bf31ebbcce52758858875256acd792
neerajbafila@gmail.com <neerajbafila@gmail.com>
2021-11-20 18:32:59 +0530

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running. By Docker CICD Done"
+    return "Flask app is running."
 
 
 if __name__ == "__main__":
